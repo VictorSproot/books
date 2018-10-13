4b513635f36c5e8dd148d0b6e66030e2c9b6f507
VictorSproot <89292844844@mail.ru>
2018-10-14 01:03:18 +0300

    Update models.py

--- booklist/models.py
+++ booklist/models.py
@@ -1,9 +1,20 @@
 from django.db import models
+from django.shortcuts import reverse
 
 CATEGORIES = (
     (1, 'Русский'),
     (2, 'Английский')
 )
+
+
+def generate_filename(instance, filename):
+    filename = instance.slug + '.pdf'
+    return "{0}/{1}".format(instance, filename)
+
+
+def generate_filename_jpg(instance, filename):
+    filename = instance.slug + '.jpg'
+    return "{0}/{1}".format(instance, filename)
 
 
 # Create your models here.
@@ -15,6 +26,12 @@ class Book(models.Model):
     keywords = models.CharField(max_length=200, blank=True, verbose_name='Кейвордс')
     category = models.ManyToManyField('Category', related_name='books', verbose_name='Категория')
     lang_category = models.IntegerField(choices=CATEGORIES, default=1, db_index=True, verbose_name='Язык')
+    book_file = models.FileField(upload_to=generate_filename, null=True, blank=True, verbose_name='Файл PDF')
+    img_file = models.ImageField(upload_to=generate_filename_jpg, null=True, blank=True, verbose_name='IMG')
+
+    def get_absolute_url(self):
+        cat_name = self.category.first().slug
+        return reverse('book_detail_url', kwargs={'slug': self.slug, 'cat_name': cat_name})
 
     class Meta:
         verbose_name = 'Книга'
@@ -27,10 +44,19 @@ class Book(models.Model):
 class Category(models.Model):
     title = models.CharField(max_length=200, db_index=True)
     slug = models.SlugField(max_length=200, db_index=True, unique=True)
+    desc_for_find_cat = models.TextField(blank=True, db_index=True, verbose_name='Описание для поиска')
+    keywords_cat = models.CharField(max_length=200, blank=True, verbose_name='Кейвордс')
+    def get_absolute_url(self):
+        return reverse('category_detail_url', kwargs={'slug': self.slug})
 
     class Meta:
         verbose_name = 'Категория'
         verbose_name_plural = 'Категории'
+        ordering = ['title']
 
     def __str__(self):
         return self.title
+
+
+
+
